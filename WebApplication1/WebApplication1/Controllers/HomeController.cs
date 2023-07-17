0ba589fe3b073a01c7ad91cb495985f5d5c92656
Abdulaziz930 <abdulazizak@code.edu.az>
2023-07-17 22:57:31 +0400

    done

---
+++ WebApplication1/WebApplication1/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            List<Student> students = new List<Student>()
+            {
+                new Student(){ Id = 1, Name = "Kenan", Surname = "Elekberov", Point = 55},
+                new Student(){ Id = 2, Name = "Elvin", Surname = "Kamilov", Point = 55},
+                new Student() {Id = 3, Name = "Ilqar", Surname = "Ebdurrehimov", Point = 35}
+            };
+
+            //string name = "Leman";
+
+            //ViewBag.Name = name;
+            //ViewData["Name"] = name;
+            //TempData["Name"] = name
+;
+            return View(students);
+            //return RedirectToAction("Test");
+        }
+
+        public IActionResult Test()
+        {
+            return View();
+        }
+    }
+}
---
+++ WebApplication1/WebApplication1/Controllers/ProductController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class ProductController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public string Detail(int id)
+        {
+            return $"Product - {id}";
+        }
+    }
+}
